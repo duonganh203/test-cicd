669103a9b7f4b2999e0f67d7dc92f56b215cced9
duonganh203 <rankill52@gmail.com>
2025-01-09 23:42:22 +0700

    hello

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 function App() {
-  const [hello, setHello] = useState(import.meta.env.APP_HELLO);
+  const [hello] = useState(import.meta.env.APP_HELLO);
   return (
     <>
       <h4>{hello}</h4>
