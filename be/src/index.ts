e2b2eae9a29eeb65c49db1b1f32235b4376c8047
duonganh203 <rankill52@gmail.com>
2025-01-14 21:25:09 +0700

    tst

--- be/src/index.ts
+++ be/src/index.ts
@@ -1,6 +1,8 @@
 import express, { Express, Request, Response } from "express";
-
+import cors from "cors";
 const app: Express = express();
+
+app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
